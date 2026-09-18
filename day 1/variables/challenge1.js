39b808c5bba462bea264871f360a45ec8951dd5b
daoud <daoud0@proton.me>
2026-09-18 11:05:26 +0100

    chalnge 1

---
+++ day 1/variables/challenge1.js
@@ -0,0 +1,9 @@
+const pseudo =  "ShadowX"
+let niveau = 12
+let age = 21
+let score = 4580 
+const pays = "maroc" 
+console.log("pseudo :" + pseudo)
+console.log("niveau :" + niveau )
+console.log("age :" + age )
+console.log("pays : + pays")
