de6e0b49762de366f0f65b2508463a9190a8588d
daoud2002 <daoud0@proton.me>
2026-09-20 16:57:14 +0100

    Add files via upload

---
+++ day 1/conditions 1/challeng2.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync")()
+let couleur =(prompt("couleur : "))
+if ( couleur == "rouge" ){
+    console.log(" arrétez-vous")
+}else if ( couleur == "oronge" ){
+    console.log(" rallentissrz ")
+}else if ( couleur == "vert"){
+    console.log("vous pouvez passer")
+} else {
+console.log("couleur invalide")
+}
+
